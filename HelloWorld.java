41df6f53a38389b96fcc4219099a8b3e58a99857
Mohamed <ouziane.mhm@gmail.com>
2021-12-14 21:48:10 +0100

    verion 5

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@
  *  Compilation:  javac HelloWorld.java
  *  Execution:    java HelloWorld
  *
- *
+ *  
  ******************************************************************************/
 
 public class HelloWorld {
@@ -10,8 +10,12 @@ public class HelloWorld {
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
         System.out.println("Hello, World");
+        
         System.out.println("Hello, World");
-        System.out.println("This the end of project");
+        for (int i=0; i<=5; i++){
+           System.out.println("This the end of project");
+        }
+        
     }
 
 }
